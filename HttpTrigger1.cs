ef466a2eed46d5a17327c5c52869cc7f83a33bce
Will M <will.morrison@pinnsg.com>
2025-07-30 16:29:25 -0400

    try making function anonymous

--- HttpTrigger1.cs
+++ HttpTrigger1.cs
@@ -15,7 +15,7 @@ public class HttpTrigger1
     }
 
     [Function("HttpTrigger1")]
-    public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
     {
         _logger.LogInformation("C# HTTP trigger function processed a request.");
         return new OkObjectResult("Welcome to Azure Functions!");
